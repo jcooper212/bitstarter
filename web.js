a3c7b620c6a8fda39ec8ddc05443b44666317b8d
jcooper212 <cooper@alum.mit.edu>
2013-12-13 05:22:41 +0000

    added read of index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var inbuf = new Buffer(256);
+inbuf = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+    response.send(inbuf.toString("utf-8",0,inbuf.length));
 });
 
 var port = process.env.PORT || 5000;
